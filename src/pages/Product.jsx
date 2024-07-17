20ebe4e0dcc7ae190df27b3eddc8181abd50f796
Damian X <dx4336969@gmail.com>
2024-07-17 08:50:57 +0000

    build product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,23 @@
+import PageNav from "../components/pageNav"
+import styles from './Product.module.css'
 function Product() {
     return (
-        <div>
-           product 
+        <div className={styles.product}>
+        <PageNav/>
+        <section>
+            <img src="/img-1.jpg" alt="dksfhei" />
+            <div>
+                <h2>About WorldWise.</h2>
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est dicta illum vero culpa cum quaerat architecto sapiente eius non soluta, molestiae nihil laborum, placeat debitis, laboriosam at fuga perspiciatis?
+
+
+                </p>
+                <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis doloribus libero sunt expedita ratione iusto, magni, id sapiente sequi officiis et.
+                </p>
+            </div>
+        </section>
         </div>
     )
 }
