d10e6fada1b419bb59b9a6b02bb64f89896e986a
Damian X <dx4336969@gmail.com>
2024-07-19 13:16:34 +0000

    build map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,23 @@
 import styles from './Map.module.css'
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 function Map() {
+  const customClass = '.leaflet-popup-content-wrapper leaflet-popup-tip leaflet-popup-content'
     return (
         <div className={styles.map}>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam recusandae dolorem dolore fugit placeat velit laudantium excepturi illum dignissimos ratione repellendus ducimus, tempore quos. Suscipit autem architecto sapiente voluptatum tempora.</p>
+          
+
+          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className={styles.leaflet} >
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]} >
+    <Popup className={styles.customClass} >
+      A pretty CSS3 popup Damian. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+          
         </div>
     )
 }
