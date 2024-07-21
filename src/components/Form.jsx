c15dd8964b13109c0b4d5ebc87c2dc8c683d6ddb
Damian X <dx4336969@gmail.com>
2024-07-21 10:05:44 +0000

    building dynamic city form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,52 @@
+import { useEffect, useState } from 'react'
 import Button from './Button'
 import styles from './Form.module.css'
+import { useSearchParams } from 'react-router-dom'
 function Form() {
+    const [retrievedCity, setRetrievedCity] = useState({})
+    
+    const [searchParams,setSearchParams] = useSearchParams();
+    const lat = searchParams.get('lat')
+    const lng = searchParams.get('lng')
+    console.log(retrievedCity)
+    const [emoji, setEmoji] = useState('')
+    function getFlagEmoji(countryCode) {
+        const codePoints =  countryCode?.toUpperCase().split("").map((char) => 127397 + char.charCodeAt(0));
+        return String.fromCodePoint(...codePoints);
+    }
+    
+
+    
+
+    useEffect(function (){
+        async function getCity(){
+            const res = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`);
+            if(!res.ok) throw new Error('Could not fetch from server')
+                const data = await res.json();
+            setRetrievedCity(data)
+             setEmoji(getFlagEmoji(data.countryCode))
+        }
+        getCity()
+    },[lat,lng])
+    
+    console.log(emoji)
+    
+
     return (
         
             <form className={styles.form} >
                 <div>
                 <label htmlFor="">City name</label>
-                <input type="text"  />
-                <span>🌟</span>
+                <input type="text" value={retrievedCity.city}  />
+                <span>{emoji}</span>
                 </div>
 
                 <div>
-                <label htmlFor="">When did you go to Vecilla, La?</label>
-                <input type="text"  />
+                <label htmlFor="">When did you go to {retrievedCity.city}</label>
+                <input type="text"   />
                 </div>
                 <div>
-                <label htmlFor="">Notes about your trip to Vecilla, La</label>
+                <label htmlFor="">Notes about your trip to {retrievedCity.city}</label>
                 <textarea  id="textarea"></textarea>
                 </div>
                 <div className={styles.btnBox}>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,32 +1,50 @@
-import { useEffect, useState } from 'react'
-import styles from './Map.module.css'
-import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from 'react-leaflet'
-import { map } from 'leaflet'
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import styles from "./Map.module.css";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useNavigate } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 function Map() {
-  const customClass = '.leaflet-popup-content-wrapper leaflet-popup-tip leaflet-popup-content'
-  
-  
-    return (
-        <div className={styles.map}>
-          
+  const customClass =
+    ".leaflet-popup-content-wrapper leaflet-popup-tip leaflet-popup-content";
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
 
-          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} className={styles.leaflet} >
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]} >
-    <Popup className={styles.customClass} >
-      A pretty CSS3 popup Damian. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-  <DetectClick/>
-  <ChangeCenter/>
-</MapContainer>
-          
-        </div>
-    )
+  return (
+    <div className={styles.map}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={8}
+        scrollWheelZoom={true}
+        className={styles.leaflet}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup className={styles.customClass}>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
+    </div>
+  );
 }
 
 function ChangeCenter({ position }) {
@@ -43,4 +61,4 @@ function DetectClick() {
   });
 }
 
-export default Map
+export default Map;
